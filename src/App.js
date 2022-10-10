b3d7483fcd0c388fc8a4bc289e4d8e94efd2cf61
Petra Söderström <petrasoderstrom1612@gmail.com>
2022-10-10 10:28:11 +0200

    tt

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import TaskList from 'components/TaskList';
 
 export const App = () => {
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    console.log('test'); /* mounting here */
+  }, []) /* first appearance of use effect
+  if empty array, the function jumps to the start of the use Effect function */
+
+  useEffect(() => {
+    window.alert(`the current counter is ${counter}`)
+  }, [counter]) /* first appearance of use effect - counter mounts directly */
+
+  const onCounterIncrease = () => {
+    setCounter(counter + 1);
+  }
+
   return (
     <div>
       Find me in src/app.js!
+      <p>{counter}</p>
+      <button onClick={onCounterIncrease} type="button"> counter increate </button>
+      {counter === 1 && (<TaskList list="Test list" />)}
     </div>
   )
 }
